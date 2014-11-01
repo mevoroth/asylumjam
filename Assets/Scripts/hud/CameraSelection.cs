6bf26707bd83b9bcb4c9b3a4464965bf5bcb5ab7
Daniel 'Mevoroth' Quach <darkskana@gmail.com>
2014-11-01 20:51:50 +0100

    Camera system, Integration and commands

---
+++ Assets/Scripts/hud/CameraSelection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSelection : MonoBehaviour {
+	public CameraSystem m_camSystem;
+	public int m_camId;
+
+	public void OnMouseDown()
+	{
+		Debug.Log("TOPLEL!CAM" + m_camId);
+		m_camSystem.SetCurrent(m_camId);
+	}
+}
--- Assets/Scripts/hud/CameraSystem.cs
+++ Assets/Scripts/hud/CameraSystem.cs
@@ -10,6 +10,10 @@ public class CameraSystem : MonoBehaviour
 	public const int MAIN_CAM_LAYER = 0;
 	public const int SUB_CAM_LAYER = 10;
 
+	public Rect m_mainCam = new Rect(0.15f, 0.15f, 0.6f, 0.6f);
+	public Rect m_subCams = new Rect(0.75f, 0.75f, 0.2f, 0.2f);
+	public float m_step = 0.25f;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -19,18 +23,32 @@ public class CameraSystem : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		m_cameras[m_current].rect = new Rect(0f, 0f, 1f, 1f);
+		m_cameras[m_current].rect = m_mainCam;
 		m_cameras[m_current].depth = MAIN_CAM_LAYER;
 
 		for (int i = 0, camCount = 0; i < m_cameras.Count; ++i)
 		{
 			if (i != m_current)
 			{
-				m_cameras[i].rect = new Rect(0.69f, 0.66f - 0.325f * camCount, 0.3f, 0.3f);
-				Debug.Log("subcam:" + (1.0f - 0.325f * camCount));
+				m_cameras[i].rect = new Rect(
+					m_subCams.x,
+					m_subCams.y - m_step * camCount,
+					m_subCams.width,
+					m_subCams.height
+				);
 				m_cameras[i].depth = SUB_CAM_LAYER;
 				++camCount;
 			}
 		}
+	}
+
+	public void SetCurrent(int current)
+	{
+		int newVal = (m_current > current ? current : current + 1);
+		if (newVal >= m_cameras.Count)
+		{
+			return;
+		}
+		m_current = newVal;
 	}
 }
